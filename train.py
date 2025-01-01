349d37d5cc81fbfd925a1e2cc837766ffd7845d8
Stephan Herrmann <st.herrmann1@gmail.com>
2025-01-01 19:10:37 +0100

    hyperdrive run works.

--- train.py
+++ train.py
@@ -61,7 +61,7 @@ def main():
     # TODO: Split data into train and test sets.
 
     ### YOUR CODE HERE ###
-    x_train, y_train = train_test_split()
+    x_train, x_test, y_train, y_test = train_test_split(x,y)
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
